f6bc673700e787d906c8d3c1bcdc5e021b0b6ed0
Aytana18 <neiramonjeaytana@gmail,com>
2026-09-18 18:25:27 -0500

    feat: agrega proyecto base de Spring Boot

---
+++ src/test/java/pe/aytana/inventario_api/InventarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.aytana.inventario_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
